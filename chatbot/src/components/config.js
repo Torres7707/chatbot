50c0fefb781eeecac76d7b60566c0715f952270d
王东(10032047) <wangd-aa@glodon.com>
2020-09-24 16:19:24 +0800

    greet&javascript

---
+++ chatbot/src/components/config.js
@@ -0,0 +1,57 @@
+// Config starter code
+import React from 'react';
+import { createChatBotMessage } from 'react-chatbot-kit';
+import LearningOptions from './LearningOptions/LearningOptions';
+import LinkList from './LinkList/LinkList';
+const config = {
+	botName: 'LearningBot',
+	initialMessages: [
+		createChatBotMessage(
+			'Hi,my man, what do you want to do? why not use google?',
+			{
+				widget: 'learningOptions',
+			}
+		),
+	],
+	customStyles: {
+		botMessageBox: {
+			backgroundColor: '#376b7e',
+		},
+		chatButton: {
+			backgroundColor: '#376B7E',
+		},
+	},
+	widgets: [
+		{
+			widgetName: 'learningOptions',
+			widgetFunc: (props) => <LearningOptions {...props} />,
+		},
+		{
+			widgetName: 'javascriptLinks',
+			widgetFunc: (props) => <LinkList {...props} />,
+			props: {
+				options: [
+					{
+						text: 'Introduction to JS',
+						url:
+							'https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/',
+						id: 1,
+					},
+					{
+						text: 'Mozilla JS Guide',
+						url:
+							'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide',
+						id: 2,
+					},
+					{
+						text: 'Frontend Masters',
+						url: 'https://frontendmasters.com',
+						id: 3,
+					},
+				],
+			},
+		},
+	],
+};
+
+export default config;
